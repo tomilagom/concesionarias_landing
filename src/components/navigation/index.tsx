6dfc3aefb5e0f3c5bd50e37472daf2688fc1ce28
Tomas <tomas@hyppo.io>
2026-03-02 18:55:03 +0100

    change logo

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -9,7 +9,7 @@ const Navigation = () => {
           <div className={tw(`flex items-center`)}>
             <div className={tw(`flex-shrink-0 flex items-center gap-3`)}>
               <div className={tw(`w-10 h-10 md:w-15 md:h-15 bg-white-600 rounded-xl flex items-center justify-center shadow-lg shadow-indigo-500/20`)}>
-                <img className={tw(`h-8 w-8 md:h-12 md:w-12`)} src="logo.svg" alt="logo" width={24} height={24} />
+                <img className={tw(`h-8 w-8 md:h-12 md:w-12`)} src="logo.png" alt="logo" width={24} height={24} />
               </div>
               <span className={tw(`text-white font-bold text-lg md:text-xl tracking-tight`)}>ConcesionarIA</span>
             </div>
